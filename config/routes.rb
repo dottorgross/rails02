dad4db5d564869c412f0b76ba31ba04ed6602a82
Andrea Ros <a.ros@donboscosandona.it>
2021-12-10 10:42:42 +0000

    creazione home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # consentiamo che il visitatore possa aprire la pagina index:
+  # (controller home, azione index)
+  get '/home/index'
+  root 'home#index'
 end
